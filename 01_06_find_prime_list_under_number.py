d8c8ec117bb1d1eaed24a42d1dc92a41075e4d46
seonghyun <codiee@naver.com>
2025-05-03 23:06:22 +0900

    1주차 알고리즘 학습

---
+++ 01_06_find_prime_list_under_number.py
@@ -0,0 +1,18 @@
+# Q. 정수를 입력 했을 때, 그 정수 이하의 소수를 모두 반환하시오.
+
+input = 20
+
+def find_prime_list_under_number(number):
+    arr=[]
+    for num in range(2,number+1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if(num % i)==0:
+                is_prime = False
+                break
+        if is_prime:
+            arr.append(num)
+    print(arr)
+
+result = find_prime_list_under_number(input)
+print(result)
