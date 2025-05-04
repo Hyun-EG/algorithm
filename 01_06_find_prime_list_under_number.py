69e1f04201163afab63742a3d38df57b3a4a1de7
seonghyun <codiee@naver.com>
2025-05-04 14:26:01 +0900

    1주차 알고리즘 학습

--- 01_06_find_prime_list_under_number.py
+++ 01_06_find_prime_list_under_number.py
@@ -5,13 +5,15 @@ input = 20
 def find_prime_list_under_number(number):
     arr=[]
 
-    for num in range(2,number+1):
-        for i in range(2, num):
-            if num % i == 0:
-                break
-        else:
-            arr.append(num)
-    print(arr)
+    # 문제 해결 2
+    # for num in range(2,number+1):
+    #     for i in arr:  # 효율적
+    #     # for i in range(2, num): 비 효율적
+    #         if i * i <= num and num % i == 0:
+    #             break
+    #     else:
+    #         arr.append(num)
+    # print(arr)
 
 
     # 문제 해결 1
