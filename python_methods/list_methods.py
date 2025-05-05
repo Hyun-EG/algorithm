6dae5bea3bb9f61cdc15eacd60d1dc0ce907628e
seonghyun <codiee@naver.com>
2025-05-06 00:47:01 +0900

    파이썬 메소드 정리(리스트)

---
+++ python_methods/list_methods.py
@@ -0,0 +1,11 @@
+
+# list.append(x) : 리스트 뒤에 입력한 x를 추가
+list_append = ['박','성']
+list_append.append("현")
+print(list_append) #['박','성','현']
+
+# list.insert(i,x) : i번째 인덱스에 입력한 x를 추가
+list_insert = ['박','현']
+list_insert.insert(1,'성')
+print(list_insert) # ['박','성','현']
+
